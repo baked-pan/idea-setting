7caba226dc885050e91f9dd09f9d18d7f7e90128
bakedpan <572943211@qq.com>
2019-05-10 01:48:25 +0800

    IU-2019.1 <pancc@pancc Create markdown.xml, manifest.json .json, print.xml, project.default.xml, Default copy.xml, Interface.java, yiiguxing.translation.xml, Default.xml, applicationLibraries.xml, file.template.settings.xml, find.xml, keymap.xml, mavenVersion.xml, javarebel.xml, bootstrap.xml, Default.xml, JavaScript.xml, Eclipse copy.xml, editor.codeinsight.xml, editor.xml, Solarized.icls, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, ide-features-trainer.xml, lombok-plugin.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, projectView.xml, Java.xml, plugin_ui.xml, colors.scheme.xml, Default copy 1.xml, databaseDrivers.xml, github.xml, templates.xml, Sublime Monokai True.icls, laf.xml, parameter.hints.xml, FadeComments.icls, Class.java, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, usageView.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author ${USER}
+ * @date ${DATE} ${TIME}
+ * @version 1.0
+ */
+public interface ${NAME} {
+}
